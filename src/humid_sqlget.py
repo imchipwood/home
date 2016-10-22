eeabff172e1d10cfbea72c7fb28bb520d2f02aee
Chip <imchipwood@gmail.com>
2016-10-21 17:23:49 -0700

    added query for "all entries" from today

--- src/humid_sqlget.py
+++ src/humid_sqlget.py
@@ -45,22 +45,28 @@ def main():
     # do SQL query and format the data
     try:
         if sQueryParsed[0] == 'nEntries':
-        
-            dbcmd = "SELECT * FROM data ORDER BY ID DESC LIMIT {}".format(sQueryParsed[1])
-            with db:
-                curs.execute( dbcmd )
-            print "\nDate       | Time     | Room     | Temperature | Humidity"
-            print "----------------------------------------------------------"
-            #for reading in curs.fetchall():
-            lData = curs.fetchall()
-            for i in reversed(xrange(len(lData))):
-                reading = lData[i]
-                date = "{}".format(reading[0])
-                time = "{0:8s}".format(reading[1])
-                room = "{0:8s}".format(reading[2])
-                temp = "{0:11.1f}".format(reading[3])
-                humi = "{0:0.1f}".format(reading[4]) + "%"
-                print date + " | " + time + " | " + room + " | " + temp + " | " + humi
+            dbcmd = "SELECT * FROM {0} ORDER BY ID DESC LIMIT {1}".format(sqlget['table'], sQueryParsed[1])
+        elif sQueryParsed[0] == 'today':
+            sDateCol = ''
+            for col in sqlget['columns']:
+                if 'date' in col.lower():
+                    sDateCol = col
+            dbcmd = "SELECT * FROM {0} WHERE {1} BETWEEN CURRENT_DATE() AND NOW() ORDER BY ID DESC".format(sqlget['table'], sDateCol)
+        with db:
+            curs.execute( dbcmd )
+        print "\nDate       | Time     | Room     | Temperature | Humidity"
+        print "----------------------------------------------------------"
+        #for reading in curs.fetchall():
+        lData = curs.fetchall()
+        for i in reversed(xrange(len(lData))):
+            reading = lData[i]
+            date = "{}".format(reading[0])
+            time = "{0:8s}".format(reading[1])
+            room = "{0:8s}".format(reading[2])
+            temp = "{0:11.1f}".format(reading[3])
+            humi = "{0:0.1f}".format(reading[4]) + "%"
+            print date + " | " + time + " | " + room + " | " + temp + " | " + humi
+            
     except KeyboardInterrupt:
         print "\n\t-e- KeyboardInterrupt, exiting gracefully\n"
         sys.exit(1)
@@ -78,16 +84,21 @@ def queryCheck():
     
     # first, split by spaces - how many args are there?
     # behave differently for 1 or 2 args
+    parsedQuery = []
     nArgs = sQuery.rstrip().lstrip().split(' ')
     if len(nArgs) < 1 or len(nArgs) > 2:
         raise Exception('-E- Too many or too few args, bruh, not sure what to do.\n\tArgs: {}'.format(sQuery))
     elif len(nArgs) == 1:
         print "-d- 1 arg"
-        # user just wants to get last N entries, let em do it...
-        return ['nEntries', int(nArgs[0])]
+        # check if user wants today or nEntries
+        if 'today' in nArgs[0].lower:
+            parsedQuery = ['today', '']
+        else:
+            parsedQuery = ['nEntries', int(nArgs[0])]
     elif len(nArgs) == 2:
         print "-d- 2 args"
-        return
+    
+    return parsedQuery
     
 
 if __name__ == '__main__':
