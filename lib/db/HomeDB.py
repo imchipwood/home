60bbb92395f113a4710a88501fa1746c019bcd87
Chip <imchipwood@gmail.com>
2016-10-24 20:57:26 -0700

    first hack at refactoring DB query interactions. Ugly so far, testing needed

---
+++ lib/db/HomeDB.py
@@ -0,0 +1,201 @@
+# SQL Library
+#
+# The intention of this file is to consolidate all 
+# SQL interactions into a single library,
+# thereby de-cluttering the scripts coordinating
+# sensor reading, data logging, etc.
+
+import os
+
+class HomeDB(object):
+    db = ''
+    curs = ''
+    dbcmd = ''
+    dataRaw = []
+    dataFormatted = []
+    
+    conf = {'db':'', 'table':'', 'user':'', 'pw':'', 'room':'', 'columns':[]}
+    sConfFile = ''
+    
+    def __init__(self, f):
+        super(HomeDB, self).__init__()
+        
+        # read config file
+        if os.path.exists(f):
+            self.sConfFile = f
+        else:
+            raise IOError('-E- HomeDB Error: Please check if the specified DB config file exists: {}'.format(f))
+        
+        if self.readConfig():
+            # open up database
+            try:
+                self.db = MySQLdb.connect('localhost', self.conf['user'], self.conf['pw'], self.conf['db'])
+                self.curs = self.db.cursor()
+            except:
+                raise IOError('Failed to open database. Please check config')
+        else:
+            raise IOError('-E- HomeDB Error: Failed to properly parse DB config file: {}'.format(self.sConfFile))
+
+    def readConfig(self):
+        confTemp = {}
+        with open(self.sConfFile, 'r') as inf:
+            for line in inf:
+                line = line.rstrip().split('=')
+                # database
+                if line[0] == 'db':
+                    confTemp['db'] = line[-1]
+                # table
+                if line[0] == 't':
+                    confTemp['table'] = line[-1]
+                # username
+                if line[0] == 'u':
+                    confTemp['user'] = line[-1]
+                # password
+                if line[0] == 'p':
+                    confTemp['pw'] = line[-1]
+                # room
+                if line[0] == 'r':
+                    confTemp['room'] = line[-1]
+                # columns to populate
+                if line[0] == 'c':
+                    confTemp['columns'] = line[-1].split(',')
+        # check for blanks
+        validConf = not '' in [confTemp['db'], confTemp['table'], confTemp['user'], confTemp['pw']]
+        if not 'get' in self.sConfFile.lower():
+            validConf = not '' in [confTemp['room']]
+            #for key, value in confTemp['columns']:
+            #    validConf = True if key != '' else False
+            #    validConf = True if value != '' else False
+            validConf = not '' in confTemp['columns']
+        if validConf:
+            self.conf = validConf
+        return validConf
+        
+    def constructQuery(self, sQuery):
+        bDebug = True
+        
+        if bDebug:
+            print '-d- Parsing query: "{}"'.format(sQuery)
+    
+        # default query if no args are specified (or if empty args specified)
+        dQuery = {}
+        dQuery['query'] = 'n'
+        dQuery['qualifier'] = '5'
+        dQuery['room'] = '*'
+        
+        lArgs = sQuery.rstrip().lstrip().split(' ')
+        if bDebug:
+            print '-d- args split into: "{}"'.format(lArgs)
+        # are they any args? If so, parse em. If not, assume default
+        if len(lArgs) > 0 and lArgs != ['']:
+            # loop thru each arg, populating the dQuery dictionary as we go
+            for sArg in lArgs:
+                # first, deconstruct the arg into a key/value pair
+                sKey = value = ''
+                sArgSplit = sArg.split('=')
+                if len(sArgSplit) == 2:
+                    sKey = sArgSplit[0].lower()
+                    value = sArgSplit[1]
+                elif len(sArgSplit) == 1:
+                    if sArgSplit[0].lower() == 'today':
+                        sKey = 'today'
+                        value = ''
+                    else:
+                        sKey = 'n'
+                        value = sArgSplit[0]
+                else:
+                    raise Exception('-E- Something\'s up with your args. Couldn\'t split them into a key/value pair\n\tArgs: {0}\n\tFailed on: {1}'.format(sQuery, sArg))
+                if bDebug:
+                    print "-d- key, value: ({}, {})".format(sKey, value)
+    
+                # room specification
+                if sKey == 'room':
+                    dQuery['room'] = '\'{}\''.format(value)
+                # nEntries
+                elif sKey == 'n':
+                    dQuery['query'] = 'n'
+                    dQuery['qualifier'] = int(value)
+                # today's entries
+                elif sKey == 'today':
+                    dQuery['query'] = 'today'
+                    dQuery['qualifier'] = ''
+                # entries for a particular date
+                elif sKey == 'date':
+                    # check syntax of date
+                    sDate = value
+                    sDateSplit = sDate.split('-')
+                    dDate = {'year':sDateSplit[0], 'month':sDateSplit[1], 'day':sDateSplit[2]}
+                    if len(dDate['year']) != 4:
+                        raise Exception('-E- Date entered incorrectly - check the year, should be 4 digits.\n\tYear: {}'.format(dDate['year']))
+                    if len(dDate['month']) != 2:
+                        raise Exception('-E- Date entered incorrectly - check the month, should be 2 digits.\n\tMonth: {}'.format(dDate['month']))
+                    if len(dDate['day']) != 2:
+                        raise Exception('-E- Date entered incorrectly - check the day, should be 2 digits.\n\tDay: {}'.format(dDate['day']))
+                    dQuery['query'] = 'date'
+                    dQuery['qualifier'] = sDate
+        
+        # extract the date column - it's used by most query types
+        sDateCol = sRoomCol = ''
+        for col in sqlget['columns']:
+            if 'date' in col.lower():
+                sDateCol = col
+            if 'room' in col.lower():
+                sRoomCol = col
+        if bDebug:
+            print "-d- Columns of interest:\n-d- date: {}\n-d- room: {}".format(sDateCol, sRoomCol)
+        
+        sRoomQuery = 'WHERE '
+        if dQuery['room'] != '*':
+            sRoomQuery += '{}={} AND'.format(sRoomCol, dQuery['room'])
+            
+        if bDebug:
+            print "-d- dQuery = {}".format(dQuery)
+            print "-d- sRoomQuery = {}".format(sRoomQuery)
+            
+        # construct query
+        if dQuery['query'] == 'n':
+            # special case for the room query
+            # 1) no ' and' at the end if a room WAS specified, and
+            # 2) if a room wasn't specified, just delete the whole thing (no need for 'WHERE')
+            sRoomQuery = sRoomQuery.replace(' AND', '') if dQuery['room'] != '*' else ''
+            dbcmd = "SELECT * FROM {0} {1} ORDER BY ID DESC LIMIT {2}".format(self.conf['table'], sRoomQuery, dQuery['qualifier'])
+        elif dQuery['query'] == 'today':
+            dbcmd = "SELECT * FROM {0} {1} {2} BETWEEN CURRENT_DATE() AND NOW() ORDER BY ID DESC".format(self.conf['table'], sRoomQuery, sDateCol)
+        elif dQuery['query'] == 'date':
+            dbcmd = "SELECT * FROM {0} {1} {2} BETWEEN '{3}' AND '{3} 23:59:59' ORDER BY ID DESC".format(self.conf['table'], sRoomQuery, sDateCol, dQuery['qualifier'])
+        if bDebug:
+            print "-d- MySQL command:\n-d- %s" % (dbcmd)
+    
+        return dbcmd
+    
+    def executeQuery(self, sqlcmd):
+        with self.db:
+            self.curs.execute(sqlcmd)
+        self.dataRaw = self.curs.fetchall()
+        
+    def formatResults(self):
+        self.dataFormatted = []
+        self.dataFormatted.append("\nDate       | Time     | Room     | Temperature | Humidity")
+        self.dataFormatted.append( "----------------------------------------------------------")
+        
+        for i in reversed(xrange(len(self.dataRaw))):
+            reading = dataRaw[i]
+            date = "{}".format(reading[0])
+            time = "{0:8s}".format(reading[1])
+            room = "{0:8s}".format(reading[2])
+            temp = "{0:11.1f}".format(reading[3])
+            humi = "{0:0.1f}".format(reading[4]) + "%"
+            self.dataFormatted.append( date + " | " + time + " | " + room + " | " + temp + " | " + humi )
+        
+    def command(self, sQuery):
+        dbcmd = self.constructQuery()
+        self.executeQuery(dbcmd)
+        
+    def displayResults(self):
+        self.formatResults()
+        for line in self.dataFormatted:
+            print line
+        
+    def getInfo(self):
+        return self.conf
+
--- src/humid_sqlget.py
+++ src/humid_sqlget.py
@@ -7,7 +7,7 @@ import traceback
 
 
 sys.path.append('/home/pi/dev/home/lib/db')
-from TableAccess import TableAccess
+from HomeDB import HomeDB
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-query', '-q', type=str, default='', help="Type of query - how and what do you want data displayed")
@@ -34,64 +34,12 @@ def main():
     if bDebug:
         print "-d- Accessing SQL DB using credentials found here:"
         print "-d- {}".format(sSQLCredentialsFile)
-    ta = TableAccess(sSQLCredentialsFile)
-    sqlget = ta.getInfo()
-    db = MySQLdb.connect('localhost', sqlget['user'], sqlget['pw'], sqlget['db'])
-    curs = db.cursor()
-
-    # extract the date column - it's used by most query types
-    sDateCol = sRoomCol = ''
-    for col in sqlget['columns']:
-        if 'date' in col.lower():
-            sDateCol = col
-        if 'room' in col.lower():
-            sRoomCol = col
-    if bDebug:
-        print "-d- Columns of interest:\n-d- date: {}\n-d- room: {}".format(sDateCol, sRoomCol)
+    hdb = HomeDB(sSQLCredentialsFile)
     
     # do SQL query and format the data
     try:
-        # figure out what query to run
-        sQueryParsed = queryCheck()
-        
-        sRoomQuery = 'WHERE '
-        if sQueryParsed['room'] != '*':
-            sRoomQuery += '{}={} AND'.format(sRoomCol, sQueryParsed['room'])
-            
-        if bDebug:
-            print "-d- sQueryParsed = {}".format(sQueryParsed)
-            print "-d- sRoomQuery = {}".format(sRoomQuery)
-            
-        # construct query
-        if sQueryParsed['query'] == 'n':
-            # special case for the room query
-            # 1) no ' and' at the end if a room WAS specified, and
-            # 2) if a room wasn't specified, just delete the whole thing (no need for 'WHERE')
-            sRoomQuery = sRoomQuery.replace(' AND', '') if sQueryParsed['room'] != '*' else ''
-            dbcmd = "SELECT * FROM {0} {1} ORDER BY ID DESC LIMIT {2}".format(sqlget['table'], sRoomQuery, sQueryParsed['qualifier'])
-        elif sQueryParsed['query'] == 'today':
-            dbcmd = "SELECT * FROM {0} {1} {2} BETWEEN CURRENT_DATE() AND NOW() ORDER BY ID DESC".format(sqlget['table'], sRoomQuery, sDateCol)
-        elif sQueryParsed['query'] == 'date':
-            dbcmd = "SELECT * FROM {0} {1} {2} BETWEEN '{3}' AND '{3} 23:59:59' ORDER BY ID DESC".format(sqlget['table'], sRoomQuery, sDateCol, sQueryParsed['qualifier'])
-        if bDebug:
-            print "-d- MySQL command:\n-d- %s" % (dbcmd)
-        # run query
-        with db:
-            curs.execute( dbcmd )
-
-        # display results
-        print "\nDate       | Time     | Room     | Temperature | Humidity"
-        print "----------------------------------------------------------"
-        lData = curs.fetchall()
-        for i in reversed(xrange(len(lData))):
-            reading = lData[i]
-            date = "{}".format(reading[0])
-            time = "{0:8s}".format(reading[1])
-            room = "{0:8s}".format(reading[2])
-            temp = "{0:11.1f}".format(reading[3])
-            humi = "{0:0.1f}".format(reading[4]) + "%"
-            print date + " | " + time + " | " + room + " | " + temp + " | " + humi
-
+        hdb.command(sQuery)
+        hdb.displayResults()
     except KeyboardInterrupt:
         print "\n\t-e- KeyboardInterrupt, exiting gracefully\n"
         sys.exit(1)
@@ -100,70 +48,6 @@ def main():
         traceback.print_exc()
         raise e
     return True
-
-def queryCheck():
-    global sQuery
-    global bDebug
-
-    if bDebug:
-        print '-d- Parsing query: "{}"'.format(sQuery)
-
-    # default query if no args are specified (or if empty args specified)
-    parsedQuery = {}
-    parsedQuery['query'] = 'n'
-    parsedQuery['qualifier'] = '5'
-    parsedQuery['room'] = '*'
-    
-    lArgs = sQuery.rstrip().lstrip().split(' ')
-    if bDebug:
-        print '-d- args split into: "{}"'.format(lArgs)
-    # are they any args? If so, parse em. If not, assume default
-    if len(lArgs) > 0 and lArgs != ['']:
-        # loop thru each arg, populating the parsedQuery dictionary as we go
-        for sArg in lArgs:
-            # first, deconstruct the arg into a key/value pair
-            sKey = value = ''
-            sArgSplit = sArg.split('=')
-            if len(sArgSplit) == 2:
-                sKey = sArgSplit[0].lower()
-                value = sArgSplit[1]
-            elif len(sArgSplit) == 1:
-                if sArgSplit[0].lower() == 'today':
-                    sKey = 'today'
-                    value = ''
-                else:
-                    sKey = 'n'
-                    value = sArgSplit[0]
-            else:
-                raise Exception('-E- Something\'s up with your args. Couldn\'t split them into a key/value pair\n\tArgs: {0}\n\tFailed on: {1}'.format(sQuery, sArg))
-            if bDebug:
-                print "-d- key, value: ({}, {})".format(sKey, value)
-
-            # room
-            if sKey == 'room':
-                parsedQuery['room'] = '\'{}\''.format(value)
-            elif sKey == 'n':
-                parsedQuery['query'] = 'n'
-                parsedQuery['qualifier'] = int(value)
-            elif sKey == 'today':
-                parsedQuery['query'] = 'today'
-                parsedQuery['qualifier'] = ''
-            # date
-            elif sKey == 'date':
-                # check syntax of date
-                sDate = value
-                sDateSplit = sDate.split('-')
-                dDate = {'year':sDateSplit[0], 'month':sDateSplit[1], 'day':sDateSplit[2]}
-                if len(dDate['year']) != 4:
-                    raise Exception('-E- Date entered incorrectly - check the year, should be 4 digits.\n\tYear: {}'.format(dDate['year']))
-                if len(dDate['month']) != 2:
-                    raise Exception('-E- Date entered incorrectly - check the month, should be 2 digits.\n\tMonth: {}'.format(dDate['month']))
-                if len(dDate['day']) != 2:
-                    raise Exception('-E- Date entered incorrectly - check the day, should be 2 digits.\n\tDay: {}'.format(dDate['day']))
-                parsedQuery['query'] = 'date'
-                parsedQuery['qualifier'] = sDate
-
-    return parsedQuery
 
 
 if __name__ == '__main__':
