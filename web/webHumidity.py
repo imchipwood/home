0503c48276a881b0d96da356f69dfe9bad48ffba
Chip <imchipwood@gmail.com>
2016-10-29 23:04:00 -0700

    hmmm...

--- web/webHumidity.py
+++ web/webHumidity.py
@@ -49,7 +49,8 @@ def printChartCode(table):
       google.load("visualization", "1", {packages:["corechart"]});
       google.setOnLoadCallback(drawChart);
       function drawChart() {
-        var data = google.visualization.arrayToDataTable([ ['Date', 'Count'], %s ]);
+        var data = google.visualization.arrayToDataTable([ ['Date', 'Humidity, Temperature'],
+%s ]);
 
         var options = {
           title: 'Google column chart',
