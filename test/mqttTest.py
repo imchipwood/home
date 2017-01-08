5f537afb46a351535d96fad43766168441b3b2ba
Chip <imchipwood@gmail.com>
2017-01-07 21:04:11 -0800

    switching garage door to mqtt

---
+++ test/mqttTest.py
@@ -0,0 +1,27 @@
+import paho.mqtt.client as paho
+ 
+def on_connect(client, userdata, flags, rc):
+    print("CONNACK received with code %d." % (rc))
+
+
+def on_publish(client, userdata, mid):
+    print("mid: "+str(mid))
+
+
+mqttHost = "192.168.1.130"
+mqttPort = 1883
+mqttTopic = "home-assistant/garage/door"
+
+client = paho.Client(client_id="garageDoor")
+client.on_connect = on_connect
+client.on_publish = on_publish
+client.connect(mqttHost, mqttPort)
+client.loop_start()
+
+(rc, mid) = client.publish(mqttTopic, "0", qos=1)
+rc
+mid
+
+client.loop_stop()
+client.unsubscribe(mqttTopic)
+client.disconnect()
