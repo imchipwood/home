8c99f6a9c877bd28e9857dd7a5c60ee8c5248cd4
Chip <imchipwood@gmail.com>
2017-01-14 16:19:22 -0800

    more camera mucking

--- test/camera.py
+++ test/camera.py
@@ -4,8 +4,15 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
-#camera.rotation = 180
+camera.rotation = 180
+camera.brightness = 50
+camera.contrast = 50
 camera.start_preview()
 sleep(5)
 camera.capture('/home/cpw/camera/captures/test000.jpg')
+
+# command to open video: omxplayer video.h264
+#camera.start_recording('/home/cpw/camera/recordings/video.h264')
+#sleep(10)
+#camera.stop_recording(
 camera.stop_preview()
