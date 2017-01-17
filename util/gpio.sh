54dadfcf4f4fc6f8e704ce23deed63ea2473733e
Chip <imchipwood@gmail.com>
2017-01-16 19:22:47 -0800

    force gpio low at boot

--- util/gpio.sh
+++ util/gpio.sh
@@ -1,3 +1,9 @@
-echo "14" > /sys/class/gpio/export
-echo "low" > /sys/class/gpio/gpio14/direction
-echo "14" > /sys/class/gpio/unexport
+#! /bin/bash
+#echo "14" > /sys/class/gpio/export
+#echo "low" > /sys/class/gpio/gpio14/direction
+#echo "14" > /sys/class/gpio/unexport
+
+# Initializes all gpio pins to output mode and sets them all LOW
+#sleep 1
+for i in `seq 0 16`; do /usr/local/bin/gpio mode $i out; done
+for i in `seq 0 16`; do /usr/local/bin/gpio write $i 0; done
