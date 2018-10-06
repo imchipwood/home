3c3382afbb639da29d537ea9e1e5fcd86a92f596
Chip <imchipwood@gmail.com>
2018-10-06 14:17:48 -0700

    simple MQTTClient object overriding the paho.mqtt.client.Client object

---
+++ library/communication/mqtt.py
@@ -0,0 +1,76 @@
+from paho.mqtt.client import Client, MQTTv311
+from paho.mqtt import publish
+
+from library.config.mqttconfig import MQTTConfig
+
+
+class MQTTClient(Client):
+	def __init__(self, client_id="", clean_session=True, userdata=None, protocol=MQTTv311, transport="tcp", mqttconfig=None):
+		"""
+		@type mqttconfig: MQTTConfig
+		"""
+		if mqttconfig:
+			super(MQTTClient, self).__init__(
+				client_id=mqttconfig.client_id,
+				clean_session=clean_session,
+				userdata=userdata,
+				protocol=protocol,
+				transport=transport
+			)
+		else:
+			super(MQTTClient, self).__init__(
+				client_id=client_id,
+				clean_session=clean_session,
+				userdata=userdata,
+				protocol=protocol,
+				transport=transport
+			)
+
+		self.config = mqttconfig
+
+	def connect(self, host="", port=1883, keepalive=60, bind_address=""):
+		"""
+		Override connect with info from config if args are not provided
+		@param host: hostname or IP address of the remote broker.
+		@type host: str or None
+		@param port: network port of the server host to connect to. Defaults to
+		1883. Note that the default port for MQTT over SSL/TLS is 8883 so if you
+		are using tls_set() the port may need providing.
+		@type port: int
+		@param keepalive: Maximum period in seconds between communications with the
+		@type keepalive: int
+		@param bind_address: Maximum period in seconds between communications with the
+		broker. If no other messages are being exchanged, this controls the
+		rate at which the client will send ping messages to the broker.
+		@type bind_address: str
+		@return: connection result
+		@rtype: int
+		"""
+		return super(MQTTClient, self).connect(
+			host=host or self.config.broker,
+			port=port or self.config.port,
+			keepalive=keepalive,
+			bind_address=bind_address
+		)
+
+	def single(self, topic, payload=None, qos=1, retain=True, hostname=None,
+			port=None, client_id="", keepalive=60, will=None, auth=None,
+			tls=None, protocol=MQTTv311, transport="tcp"):
+		"""
+		paho.mqtt.publish.single method
+		"""
+		publish.single(
+			topic=topic,
+			payload=payload,
+			qos=qos,
+			retain=retain,
+			hostname=hostname or self.config.broker,
+			port=port or self.config.port,
+			client_id=client_id or self._client_id,
+			keepalive=keepalive,
+			will=will,
+			auth=auth,
+			tls=tls,
+			protocol=protocol,
+			transport=transport
+		)
