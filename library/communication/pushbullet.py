0d2083518113f75be84e76d7e72dcfcb96503bdf
imchipwood <imchipwood@gmail.com>
2019-09-17 13:00:26 -0700

    pushbullet needed to be upgraded!

--- library/communication/pushbullet.py
+++ library/communication/pushbullet.py
@@ -6,7 +6,7 @@ from pushbullet import Pushbullet
 class PushbulletImageNotify(Pushbullet):
 	def __init__(self, api_key, file_path):
 		super(PushbulletImageNotify, self).__init__(api_key)
-		file_name = os.path.basename(file_path).encode("utf-8")
+		file_name = os.path.basename(file_path)
 		with open(file_path, 'rb') as pic:
 			file_data = self.upload_file(pic, file_name)
 		self.result = self.push_file(**file_data)
