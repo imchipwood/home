8d7d91bdce9512fc663fff2f40c217327a3f839e
imchipwood <imchipwood@gmail.com>
2020-03-08 17:30:05 -0700

    merging home_monitor_script into master

--- util/home_monitor.sh
+++ util/home_monitor.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ALIVE="$(ps aux | grep venv3.6 | grep python)"
-echo "${ALIVE}"
-if [-s "$ALIVE"] then
-  echo "home.service down - restarting"
-  systemctl restart home.service
+ALIVE="$(ps aux | grep -v "grep" | grep python)"
+if [ -z "$ALIVE" ]
+then
+  echo "\nhome.service down - restarting"
+  eval "$(systemctl restart home.service)"
 else
-  echo "5home.service alive - exiting"
+  echo "\nhome.service alive - exiting"
 fi
 exit 0
